eae96a8ab98a12e4a884a44cc24974dd958be64e
Jean Yves Ehouman <yvesehouman09@gmail.com>
2024-10-20 02:43:17 +0900

    Completed stock

--- app/controllers/admin/stocks_controller.rb
+++ app/controllers/admin/stocks_controller.rb
@@ -3,7 +3,7 @@ class Admin::StocksController < ApplicationController
 
   # GET /admin/stocks or /admin/stocks.json
   def index
-    @admin_stocks = Stock.all
+    @admin_stocks = Stock.where(product_id: params[:product_id])
   end
 
   # GET /admin/stocks/1 or /admin/stocks/1.json
@@ -18,15 +18,19 @@ class Admin::StocksController < ApplicationController
 
   # GET /admin/stocks/1/edit
   def edit
+    @product = Product.find(params[:product_id])
+    @admin_stock = Stock.find(params[:id])
   end
 
   # POST /admin/stocks or /admin/stocks.json
   def create
-    @admin_stock = Stock.new(admin_stock_params)
+    @product = Product.find(params[:product_id])
+    @admin_stock = @product.stocks.new(admin_stock_params)
 
     respond_to do |format|
       if @admin_stock.save
-        format.html { redirect_to @admin_stock, notice: "Stock was successfully created." }
+        format.html { redirect_to admin_product_stock_url(@admin_stock.product, @admin_stock), 
+          notice: "Stock was successfully created." }
         format.json { render :show, status: :created, location: @admin_stock }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -37,9 +41,11 @@ class Admin::StocksController < ApplicationController
 
   # PATCH/PUT /admin/stocks/1 or /admin/stocks/1.json
   def update
+    @product = Product.find(params[:product_id])
     respond_to do |format|
       if @admin_stock.update(admin_stock_params)
-        format.html { redirect_to @admin_stock, notice: "Stock was successfully updated." }
+        format.html { redirect_to admin_product_stock_url(@product, @admin_stock), 
+          notice: "Stock was successfully updated." }
         format.json { render :show, status: :ok, location: @admin_stock }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,7 +59,7 @@ class Admin::StocksController < ApplicationController
     @admin_stock.destroy!
 
     respond_to do |format|
-      format.html { redirect_to admin_stocks_path, status: :see_other, notice: "Stock was successfully destroyed." }
+      format.html { redirect_to admin_product_stock_url, status: :see_other, notice: "Stock was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -66,6 +72,6 @@ class Admin::StocksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def admin_stock_params
-      params.require(:stock).permit(:product_id, :amount, :size)
+      params.require(:stock).permit(:amount, :size)
     end
 end
